530726c2d50b9b778ffd7cc90103146a21a32f99
localdev0 <bluerblx160@gmail.com>
2025-02-19 12:39:06 -0500

    Create FF2.lua

---
+++ FF2.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 8204899140 then --// Public
+    loadstring(game:HttpGet('https://gist.githubusercontent.com/localdev0/ca5b3e87fed6ad7d117cb522c2e986ea/raw/7ac28001c5004c5af0d020dbcee3dff3bc6c112d/gistfile1.txt', true))()
+elseif game.PlaceId == 8206123457 then --// Practice
+    loadstring(game:HttpGet('https://gist.githubusercontent.com/localdev0/964f85e90b55f93b394b881a36ee9df8/raw/bffbdcc58db54ab8830a0eb6ea9e707bb8a954a4/gistfile1.txt', true))()
+end
